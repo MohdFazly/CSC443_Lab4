023535e4225f2ccaf60818b4428028c71157a207
MohdFazly <2020819344@student.uitm.edu.my>
2022-04-27 11:43:19 +0800

    all third commit

--- handle_form.php
+++ handle_form.php
@@ -53,8 +53,7 @@ if (isset($_POST["gender"]))
 
 if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"]) 
     { echo "Thank you," .$_POST["name"]. " for the following comments: <br /> <tt>" 
-    $_POST["comments"].
-    " </tt> <p> We will reply to you at <i>" . $_POST["email"]. 
+    $_POST["comments"]; " </tt> <p> We will reply to you at <i>" . $_POST["email"]. 
     "</i></p>"; 
     echo $message;
     }
