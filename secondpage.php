0d8049f28292b1a5e2a560122b7d235c09256190
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-22 15:08:22 +0800

    fff

--- secondpage.php
+++ secondpage.php
@@ -8,15 +8,9 @@
 </head>
 <body>
 <?php
-$$num == 22 % 2;
-if ($num = 2 || $num = 3 || $num = 4){
- 	echo "This is not the number"+" you're looking for";
-	}
-else if ($num = 5){
-echo "A snack is on its way to you!";
-}
-else
-echo "Ahaaaa Yes!";
+$a = "PHP";
+$a = $a + 1;
+echo $a;
 ?>
 
 </body>
