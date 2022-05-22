ccdeb9204f93f053bb9fe2ffa29b77f9b25ea2ae
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-22 16:07:50 +0800

    RRR

--- secondpage.php
+++ secondpage.php
@@ -7,18 +7,10 @@
     <title>Document</title>
 </head>
 <body>
+<?php include ("header.inc"); ?>
 
-<?php
-$$num = 22 % 2;
-if ($num = 2 || $num = 3 || $num = 4){
- 	echo "This is not the number"+" you're looking for";
-	}
-else if ($num = 5){
-echo "A snack is on its way to you!";
-}
-else
-echo "Ahaaaa Yes!";
-?>
+<br> <br>
+<a href="mainpage.php"> Return to Main Page </a>
 
 </body>
 </html>
